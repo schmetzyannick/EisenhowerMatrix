e0f6936c32151c6c65d07c10241cc0d00c891652
Yannick Luc Schmetz <schmetzyannick@gmail.com>
2021-10-14 20:37:07 +0200

    Finish basic cards

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend');
+    expect(app.titleLeftAddSection).toEqual('frontend');
   });
 
   it('should render title', () => {
