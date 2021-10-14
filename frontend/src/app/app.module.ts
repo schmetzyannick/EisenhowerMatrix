1dc7a434c6a4fc31c06f0792b4c595f03e61d061
Yannick Luc Schmetz <schmetzyannick@gmail.com>
2021-10-14 18:17:16 +0200

    Basic setup

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import { MatrixSectionComponent } from './matrix-section/matrix-section.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MatrixSectionComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
