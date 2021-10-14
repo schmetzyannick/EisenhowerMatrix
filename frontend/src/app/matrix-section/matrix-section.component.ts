3f9869e4411f6a8e7300dc6f4b61f9df48deaf11
Yannick Luc Schmetz <schmetzyannick@gmail.com>
2021-10-14 18:17:16 +0200

    Basic setup

---
+++ frontend/src/app/matrix-section/matrix-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-matrix-section',
+  templateUrl: './matrix-section.component.html',
+  styleUrls: ['./matrix-section.component.scss'],
+})
+export class MatrixSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
