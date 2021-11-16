07f85a0e4a76231f26fe3934624b0922fd5072f2
Yannick Luc Schmetz <schmetzyannick@gmail.com>
2021-11-16 20:30:21 +0100

    Start to implement dbcon

---
+++ backend/src/database/Database.ts
@@ -0,0 +1,32 @@
+import {Sequelize} from "sequelize";
+import {EnvironementUtils} from "../utils/EnvironementUtils";
+import {Task} from "./Task";
+
+export class Database {
+    public static getInstance(): Database {
+        if (Database.instance === undefined) {
+            Database.instance = new Database();
+        }
+        return Database.instance;
+    }
+
+    private static instance: Database;
+
+    public dbInstance: Sequelize;
+
+    private constructor() {
+        const envUtils = EnvironementUtils.getInstance();
+        this.dbInstance = new Sequelize(
+            envUtils.getPostgresDatabase(),
+            envUtils.getPostgresUser(),
+            envUtils.getPostgresPassword(),
+            {
+                host: envUtils.getPostgresHost(),
+                port: envUtils.getPostgresPort(),
+                dialect: "postgres",
+                logging: true,
+            },
+        );
+        Task.initialize(this.dbInstance);
+    }
+}
--- backend/src/utils/EnvironementUtils.ts
+++ backend/src/utils/EnvironementUtils.ts
@@ -1,5 +1,8 @@
 import nconf from "nconf";
 import path from "path";
+import {Logger} from "./Logger";
+import base64 from "base-64";
+
 /**
  * Singeton class to get the current environment.
  */
@@ -19,15 +22,50 @@ export class EnvironementUtils {
      * Get postgres host.
      */
     public getPostgresHost(): string {
-        return nconf.get("postgresHost") || "localhost";
+        const host = nconf.get("postgresHost") || "localhost";
+        Logger.getLogger().info("Postgres host", host);
+        return host;
     }
 
     /**
      * Get postgres port.
      */
     public getPostgresPort(): number {
-        return nconf.get("postgresPort") || 5432;
+        const port = nconf.get("postgresPort") || 5432;
+        Logger.getLogger().info("Postgres port", port);
+        return port;
     }
+
+    /**
+     * Get postgres database.
+     */
+    public getPostgresDatabase(): string {
+        const database = nconf.get("postgresDatabase") || "postgres";
+        Logger.getLogger().info("Postgres database", database);
+        return database;
+    }
+
+    /**
+     * Get postgres user.
+     */
+    public getPostgresUser(): string {
+        const user = nconf.get("postgresUser") || "postgres";
+        Logger.getLogger().info("Postgres user", user);
+        return user;
+    }
+
+    /**
+     * Get postgres password.
+     */
+    public getPostgresPassword(): string {
+        const password = nconf.get("postgresPassword") || "postgres";
+        if(password === "postgres") {
+            return password;
+        }else{
+            return base64.decode(password);
+        }
+    }
+
 
     protected constructor() {
         nconf.file({file: path.join(__dirname, "env.json")});
