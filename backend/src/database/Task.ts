87b69cfd179de8f20458d29d959454bb56aa13cc
Yannick Luc Schmetz <schmetzyannick@gmail.com>
2022-01-16 15:25:31 +0100

    Adds tasklist and users table

--- backend/src/database/Task.ts
+++ backend/src/database/Task.ts
@@ -1,36 +1,69 @@
-import Sequelize, { Model } from "sequelize";
+import Sequelize, {Model} from "sequelize";
+import { TaskList } from "./TaskList";
 
-export class Task extends Model{
+export class Task extends Model {
+    /**
+     * Counter in the database.
+     */
     public id!: number;
+    /**
+     * Frountend Id.
+     */
     public name!: string;
+    /**
+     * Text set by user.
+     */
     public description!: string;
+    /**
+     * Done or not.
+     */
     public status!: string;
+    /**
+     * Done or not.
+     */
+    public taskListId!: number;
+    /**
+     * First save timestamp.
+     */
     public createdAt!: Date;
+    /**
+     * Last save timestamp.
+     */
     public updatedAt!: Date;
 
     public static initialize(sequelize: any): void {
-        this.init({
-            id: {
-                type: Sequelize.INTEGER,
-                allowNull: false,
-                autoIncrement: true,
-                primaryKey: true
+        this.init(
+            {
+                id: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                    autoIncrement: true,
+                    primaryKey: true,
+                },
+                name: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+                description: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+                status: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+                taskListId: {
+                    type: Sequelize.INTEGER,
+                    references: {
+                        model: TaskList,
+                        key: 'id',
+                    }
+                }
             },
-            name: {
-                type: Sequelize.STRING,
-                allowNull: false
+            {
+                sequelize,
+                tableName: "tasks",
             },
-            description: {
-                type: Sequelize.STRING,
-                allowNull: false
-            },
-            status: {
-                type: Sequelize.STRING,
-                allowNull: false
-            }
-        }, {
-            sequelize,
-            tableName: 'tasks'
-        });
+        );
     }
 }
